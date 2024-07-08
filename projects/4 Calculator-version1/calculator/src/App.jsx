4aaa07e003bf6fe50c019185758e6b9585b973b6
Hammad-Jilani <hjilani15@gmail.com>
2024-07-08 19:39:14 +0500

    1st Commit

---
+++ projects/4 Calculator-version1/calculator/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import style from "./App.module.css";
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
+function App() {
+  return (
+    <div className={`${style["calculator"]}`}>
+      <Display></Display>
+      <ButtonsContainer></ButtonsContainer>
+    </div>
+  );
+}
+
+export default App;
---
+++ projects/4 Calculator-version1/calculator/src/components/Display.jsx
@@ -0,0 +1,10 @@
+import style from "./Display.module.css";
+function Display() {
+  return (
+    <>
+      <input type="text" className={`${style["display"]} `} />
+    </>
+  );
+}
+
+export default Display;
